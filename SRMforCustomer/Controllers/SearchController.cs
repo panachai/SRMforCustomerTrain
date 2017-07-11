6e39bc26a786e69b21a450fc7c07321a94c72b92
Panachai <panachai.ny@gmail.com>
2017-07-11 18:07:53 +0700

    Search Page
    Added SearchController
    Added View Search
    Splite Nav from _layout

---
+++ SRMforCustomer/Controllers/SearchController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using SRMforCustomer.Models;
+
+namespace SRMforCustomer.Controllers {
+    public class SearchController : Controller {
+        // GET: Search
+        [Route("Search/{reTicketID?}")]
+        public ActionResult Index(int? reTicketID) {
+            if (reTicketID != null) {
+                var requestsModel = new List<RequestsModel>() {
+                new RequestsModel(){
+                    ReTopicName = "testSRMtopic"
+                },
+                new RequestsModel(){
+                    ReTopicName = "testSRMtopic2"
+                }
+                };
+
+                return View(requestsModel);
+
+            }
+
+            return View();
+        }
+    }
+}
