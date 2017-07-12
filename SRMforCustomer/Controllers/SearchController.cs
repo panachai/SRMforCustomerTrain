c1b571bd7fb1fac54b27c29526d20297f24629dc
Panachai <panachai.ny@gmail.com>
2017-07-12 16:00:26 +0700

    Ajax completed -> Search

--- SRMforCustomer/Controllers/SearchController.cs
+++ SRMforCustomer/Controllers/SearchController.cs
@@ -9,25 +9,47 @@ namespace SRMforCustomer.Controllers {
     public class SearchController : Controller {
         // GET: Search
         public ActionResult Index() {
-        
-
             return View();
         }
 
-        void CheckTicket(int? reTicketID) {
-            if (reTicketID != null) {
-                var requestsModel = new List<RequestsModel>() {
-                    new RequestsModel(){
-                        ReTopicName = "testSRMtopic"
-                    },
-                    new RequestsModel(){
-                        ReTopicName = "testSRMtopic2"
-                    }
+        public PartialViewResult ResultSearchPartial(string keyword) {
+            if (keyword != "") {
+
+                //ยิงเซอร์วิสเอาข้อมูล 1model ยัดใส่ด้านล่าง
+
+                var selected = new RequestsModel() {
+                    ReTicketID = 1000000000,
+                    StatusID = 0,
+                    ReTopicName = "testSRMtopic : key : " + keyword + " : endKey ",
+                    ReCustomerName = "CustomerName Test",
+                    ReCustomerTel = "0900000000",
+                    ReEmail = "panachai.ny@gmail.com",
+                    ReDetail = "adasdasd Detail it here",
+                    ReDateIn = DateTime.Now,
+                    ReDateOut = DateTime.Now,
+
                 };
-
-                //return View(requestsModel);
-
+                return PartialView(selected);
             }
+
+            return PartialView();
         }
+
+
+        //void CheckTicket(int? reTicketID) {
+        //    if (reTicketID != null) {
+        //        var requestsModel = new List<RequestsModel>() {
+        //            new RequestsModel(){
+        //                ReTopicName = "testSRMtopic"
+        //            },
+        //            new RequestsModel(){
+        //                ReTopicName = "testSRMtopic2"
+        //            }
+        //        };
+
+        //        //return View(requestsModel);
+
+        //    }
+        //}
     }
 }
