ca0041afd6dc919d25884f55010b60d94735cd21
bankkingkub <thanaphan.w@prism.co.th>
2017-07-13 16:29:31 +0700

    Email  v1.0
    inw

---
+++ SRMforCustomer/Models/Config.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SRMforCustomer.Models {
+    public class Config {
+        private static string _SMTPHost;
+        private static string _DeveloperEmail;
+        public static string SMTPHost {
+            get {
+                if (_SMTPHost == null) {
+                    _SMTPHost = System.Configuration.ConfigurationManager.AppSettings["SMTPHost"];
+                }
+                return _SMTPHost;
+            }
+        }
+        public static string DeveloperEmail {
+            get {
+                if (_DeveloperEmail == null) {
+                    _DeveloperEmail = System.Configuration.ConfigurationManager.AppSettings["DeveloperEmail"];
+                }
+                return _DeveloperEmail;
+            }
+        }
+    }
+}
