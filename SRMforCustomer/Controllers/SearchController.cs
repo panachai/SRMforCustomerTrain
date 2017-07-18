3b4c51a9676f4e19e5661360773393e7750d7df9
panachai <panachai.ny@gmail.com>
2017-07-18 11:53:08 +0700

    added condition search

--- SRMforCustomer/Controllers/SearchController.cs
+++ SRMforCustomer/Controllers/SearchController.cs
@@ -13,27 +13,35 @@ namespace SRMforCustomer.Controllers {
         }
 
         public PartialViewResult ResultSearchPartial(string keyword) {
+            int ticketSearch;
             if (!string.IsNullOrEmpty(keyword)) {
-                if (Helper.Util.IsValidOTP(keyword) && keyword.Length == 7) {//checksum 9700156 (test)
-                    //ยิงเซอร์วิสเอาข้อมูล 1model ยัดใส่ด้านล่าง
+                if (int.TryParse(keyword, out ticketSearch)) {
+                    if (Helper.Util.IsValidOTP(keyword) && keyword.Length == 7) {//checksum 9700156 (test)
 
-                    var selected = new Requests() {
-                        TicketId = 1000000000,
-                        StatusId = 0,
-                        TopicName = "testSRMtopic : key : " + keyword + " : endKey ",
-                        CustomerName = "CustomerName Test",
-                        TelephoneNumber = "0900000000",
-                        Email = "panachai.ny@gmail.com",
-                        Remark = "adasdasd Detail it here",
-                        DateCreate = DateTime.Now,
-                        DateFinish = DateTime.Now,
+                        //ยิงเซอร์วิสเอาข้อมูล 1model ยัดใส่ด้านล่าง
 
-                    };
-                    return PartialView(selected);
+                        var selected = new Requests() {
+                            TicketId = 1000000000,
+                            StatusId = 0,
+                            TopicName = "testSRMtopic : key : " + keyword + " : endKey ",
+                            CustomerName = "CustomerName Test",
+                            TelephoneNumber = "0900000000",
+                            Email = "panachai.ny@gmail.com",
+                            Remark = "adasdasd Detail it here",
+                            DateCreate = DateTime.Now,
+                            DateFinish = DateTime.Now,
+
+                        };
+                        return PartialView(selected);
+                    } else {
+                        ViewBag.ErrorChecksum = "Ticket ของท่านไม่ถูกต้อง";
+                        return PartialView();
+                    }
                 } else {
-                    ViewBag.ErrorChecksum = "Ticket ของท่านไม่ถูกต้อง";
+                    ViewBag.ErrorChecksum = "โปรดป้อนเฉพาะตัวเลข";
+                    return PartialView();
                 }
-
+                ViewBag.ErrorChecksum = "โปรดป้อนข้อมูล";
 
             }
 
