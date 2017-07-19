be08620deea9d1a7d0617312ca674f050cb5e114
panachai <panachai.ny@gmail.com>
2017-07-19 10:06:56 +0700

    - Added CSS in hint on Requests - Added Varidation on RequestMetadata - Moved Position hint error on Requests

--- SRMforCustomer/Models/RequestMetadata.cs
+++ SRMforCustomer/Models/RequestMetadata.cs
@@ -19,13 +19,13 @@ namespace SRMforCustomer.Models {
         [Required]
         public int StatusId { get; set; }
         [Required]
+        [StringLength(250, MinimumLength = 3, ErrorMessage = "ป้อนไม่ถูกต้อง")]
         public string TopicName { get; set; }
         [Required]
         [StringLength(100, MinimumLength = 5, ErrorMessage = "ป้อนชื่อตั้งแต่ 5 ถึง 100ตัวอักษร")]
         public string CustomerName { get; set; }
         [Required]
-        //[MaxLength(10,"asd")]
-
+        [StringLength(10, MinimumLength = 10, ErrorMessage = "อย่างน้อย 10ตัวอักษร")]
         [Phone(ErrorMessage = "ป้อนเบอร์โทรศัพท์ให้ถูกต้อง")]
         public string TelephoneNumber { get; set; }
         [Required]
@@ -33,9 +33,9 @@ namespace SRMforCustomer.Models {
         [StringLength(50, MinimumLength = 5, ErrorMessage = "ป้อน Email ตั้งแต่ 5 ถึง 50ตัวอักษร")]
         public string Email { get; set; }
         [Required]
-        [StringLength(2000, MinimumLength = 10, ErrorMessage = "โปรกรอกอย่างน้อย 10 ตัวอักษร แต่ไม่เกิน 2000ตัวอักษร")]
+        [StringLength(2000, MinimumLength = 10, ErrorMessage = "โปรดกรอกอย่างน้อย 10 ตัวอักษร แต่ไม่เกิน 2000ตัวอักษร")]
         public string Remark { get; set; }
-        [Required]
+        
         public int CurrentStaffId { get; set; }
         [Required]
         public System.DateTime DateCreate { get; set; }
